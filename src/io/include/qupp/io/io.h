5e6cd2cef6646a6159eeba360ca87f9625edcb34
Tony Gorez <gorez.tony@gmail.com>
2023-09-30 10:21:36 +0200

    feat: add basic cursor movement (#2)

--- src/io/include/qupp/io/io.h
+++ src/io/include/qupp/io/io.h
@@ -6,15 +6,23 @@ namespace qupp::io {
 struct TerminalInterface {
   virtual ~TerminalInterface() = default;
 
-  virtual void print(const std::string &message) = 0;
+  virtual void print(const std::string &message, bool break_line) = 0;
   virtual std::string read() = 0;
+  virtual void move_cursor_left(int n) = 0;
+  virtual void move_cursor_right(int n) = 0;
+  virtual void move_cursor_up(int n) = 0;
+  virtual void move_cursor_down(int n) = 0;
 };
 
 struct Terminal : public TerminalInterface {
   static Terminal &get_instance();
 
-  void print(const std::string &message);
+  void print(const std::string &message, bool break_line);
   std::string read();
+  void move_cursor_left(int n);
+  void move_cursor_right(int n);
+  void move_cursor_up(int n);
+  void move_cursor_down(int n);
 
 private:
   // This is a key part of the Singleton Pattern; it prevents the creation of
--- src/io/unix/io.cc
+++ src/io/unix/io.cc
@@ -8,9 +8,17 @@ Terminal &Terminal::get_instance() {
   return instance;
 }
 
-void Terminal::print(const std::string &message) {
-  std::cout << message << std::endl;
+void Terminal::print(const std::string &message, bool break_line) {
+  std::cout << message;
+  if (break_line) {
+    std::cout << std::endl;
+  }
 }
+
+void Terminal::move_cursor_left(int n) { std::cout << "\033[" << n << "D"; }
+void Terminal::move_cursor_right(int n) { std::cout << "\033[" << n << "C"; }
+void Terminal::move_cursor_up(int n) { std::cout << "\033[" << n << "A"; }
+void Terminal::move_cursor_down(int n) { std::cout << "\033[" << n << "B"; }
 
 std::string Terminal::read() {
   std::string input;
